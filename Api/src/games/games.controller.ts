435372ef41387c90320fda079fb6c00b0a318ca3
Matthias Brumant <matthias.brumant@gmail.com>
2023-12-12 16:43:17 +0100

    get et delete du crud ajouté

--- Api/src/games/games.controller.ts
+++ Api/src/games/games.controller.ts
@@ -2,33 +2,35 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { GamesService } from './games.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { DataSource } from 'typeorm';
+import { InjectDataSource } from '@nestjs/typeorm';
 
 @Controller('games')
 export class GamesController {
-  constructor(private readonly gamesService: GamesService) {}
+  constructor(@InjectDataSource() private bdd : DataSource) {}
 
   @Post()
   create(@Body() createGameDto: CreateGameDto) {
-    return this.gamesService.create(createGameDto);
+    return this.bdd.query();
   }
 
   @Get()
   findAll() {
-    return this.gamesService.findAll();
+    return this.bdd.query(`SELECT * FROM game`);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.gamesService.findOne(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.gamesService.findOne(+id);
+  // }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateGameDto: UpdateGameDto) {
-    return this.gamesService.update(+id, updateGameDto);
+    return this.bdd.query();
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.gamesService.remove(+id);
+    return this.bdd.query(`DELETE FROM game WHERE id = ?`, [id]);
   }
 }
